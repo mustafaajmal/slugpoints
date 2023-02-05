9471e29f0e3a8726a3a98487a9c9d6cb67b9a42e
DancyPancy <jac.wong.k@gmail.com>
2023-02-05 00:50:02 -0800

    poopy

--- daysleft.js
+++ daysleft.js
@@ -1,3 +1,36 @@
+
+var ending_dates = ['03/24/23',
+                    '06/15/23',
+                    '12/15/23',
+                    '03/22/24',
+                    '06/13/24',
+                    '12/13/24',
+                    '03/14/25',
+                    '06/12/25',
+                    '12/12/25',
+                    '03/20/26',
+                    '06/11/26'];
+
+var starting_dates = ['01/06/23',
+                      '04/03/23',
+                      '09/23/23',
+                      '01/05/24',
+                      '04/01/24',
+                      '09/21/24',
+                      '01/03/25',
+                      '03/31/25',
+                      '09/20/25',
+                      '01/05/26', 
+                      '03/30/26'];
+
+function makeDates(list) {
+    var date_list = [];
+    for (let i = 0; i < list.length; i++) {
+        date_list.push(new Date(list[i]))
+    };
+    return date_list;
+}
+
 var daysleft = function (starts, ends) {
     const date = new Date();
     curr_start = starts[0];
@@ -14,3 +47,5 @@ var daysleft = function (starts, ends) {
         return 0;
     }
 }
+
+document.write(daysleft(makeDates(starting_dates), makeDates(ending_dates))); 
