b631551f4c27d678a9855f2af360936682542a81
DancyPancy <jac.wong.k@gmail.com>
2023-02-04 21:38:25 -0800

    implemented date script

---
+++ date.js
@@ -0,0 +1,26 @@
+var getday = function(){
+    today = new Date();
+    var date = "";
+    var month = "";
+    var dd = today.getDate()
+    var mm = today.getMonth(); //As January is 0.
+    var yyyy = today.getFullYear();
+    var monthList = ['Janurary', 'February', 'March', 
+                    'April', 'May', 'June', 'July', 
+                    'August', 'September', 'October', 
+                    'November', 'December'];
+    suff = dd % 10;
+if ((4 <= suff && suff <= 9) || suff == 0 || (11 <= dd && dd <= 19)) {
+    suff = 'th';
+} else if (suff == 1) {
+    suff = 'st';
+} else if (suff == 2) {
+    suff = 'nd';
+} else {
+    suff = 'rd';
+}
+
+    date = `${monthList[mm]} ${dd + suff}, ${yyyy}`; 
+    return (date);
+};
+document.getElementById("date").innerHTML = getday();
